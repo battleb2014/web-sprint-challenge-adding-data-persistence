def312fb45403a940b8f51e2a7e54e3fb03eb09c
Brandon May <battleb2014@gmail.com>
2021-11-04 22:44:42 -0400

    index.js and server.js done

--- api/server.js
+++ api/server.js
@@ -1,1 +1,19 @@
-// build your server here and require it from index.js
+const express = require('express');
+const server = express();
+
+const projectRouter = require('./project/router');
+const resourceRouter = require('./resource/router');
+const taskRouter = require('./task/router');
+
+server.use('/api/projects', projectRouter);
+server.use('/api/resources', resourceRouter);
+server.use('/api/tasks', taskRouter);
+
+server.use((err, req, res) => {
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack
+    })
+})
+
+module.exports = server;
